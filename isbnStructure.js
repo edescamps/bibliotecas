0f5b8ea46cbcd63d4999429dc8286690f3e8cf9d
edescamps <edescamps@users.noreply.github.com>
2016-08-18 17:48:04 -0600

    Changed camera plugin.

---
+++ isbnStructure.js
@@ -0,0 +1,48 @@
+getBookInfo({
+	"ISBN:0910187053": {
+		"publishers": [{"name": "Economics Press"}],
+	 	"pagination": "128 p. ;",
+	  	"identifiers": {"lccn": ["85243013"],
+	   	"openlibrary": ["OL2657981M"],
+	    "isbn_10": ["0910187053"],
+	    "goodreads": ["4665858"]},
+	    "subtitle": "how to work toward a constructive agreement",
+	    "title": "Negotiating techniques",
+	    "url": "https://openlibrary.org/books/OL2657981M/Negotiating_techniques",
+	    "classifications": {"dewey_decimal_class": ["658"],
+	  	"lc_classifications": ["HD58.6 .N434 1985"]},
+	   	"number_of_pages": 128,
+	    "publish_date": "1985",
+	    "key": "/books/OL2657981M",
+	    "by_statement": "Robert F. Guder, editor.",
+	   	"publish_places": [{"name": "Fairfield, N.J"}]
+	}
+});
+getBookInfo({
+	"ISBN:9780300115468": {
+		"publishers": [{"name": "Yale University Press"}], 
+		"identifiers": {"openlibrary": ["OL10319288M"], 
+		"isbn_13": ["9780300115468"], 
+		"isbn_10": ["0300115466"], 
+		"oclc": ["80180903"], 
+		"librarything": ["4794647"], 
+		"goodreads": ["51985"]}, 
+		"weight": "5.6 ounces", 
+		"title": "Existentialism Is a Humanism", 
+		"url": "https://openlibrary.org/books/OL10319288M/Existentialism_Is_a_Humanism", 
+		"number_of_pages": 128, 
+		"cover": {
+			"small": "https://covers.openlibrary.org/b/id/2355635-S.jpg", 
+			"large": "https://covers.openlibrary.org/b/id/2355635-L.jpg", 
+			"medium": "https://covers.openlibrary.org/b/id/2355635-M.jpg"
+		}, 
+		"publish_date": "July 24, 2007", 
+		"key": "/books/OL10319288M", 
+		"authors": [
+			{
+				"url": "https://openlibrary.org/authors/OL117592A/Jean-Paul_Sartre", 
+				"name": "Jean-Paul Sartre"
+			}
+		]
+	}
+});
