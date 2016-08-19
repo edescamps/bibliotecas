c78f46900a74a04d9958b7cfb747449d91284871
edescamps <edescamps@users.noreply.github.com>
2016-08-18 18:12:44 -0600

    Added phone plugin properties.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -293,27 +293,23 @@ function scanISBN () {
     
     var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-    scanner.scan( function (result) { 
+    scanner.scan( function (result) {
 
-        alert("We got a barcode\n" + 
-        "Result: " + result.text + "\n" + 
-        "Format: " + result.format + "\n" + 
-        "Cancelled: " + result.cancelled);  
+    	$('#isbn').val(result.text)
 
-       console.log("Scanner result: \n" +
-            "text: " + result.text + "\n" +
-            "format: " + result.format + "\n" +
-            "cancelled: " + result.cancelled + "\n");
-        document.getElementById("info").innerHTML = result.text;
-        console.log(result);
-        /*
-        if (args.format == "QR_CODE") {
-            window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-        }
-        */
+    	objectName = 'ISBN:' + $('#isbn').val();
+	  	$('#testBookAPI').html( '<script src="https://openlibrary.org/api/books?bibkeys=ISBN:' + $('#isbn').val() + '&jscmd=data&callback=getBookInfo"></script>');
 
-    }, function (error) { 
-        console.log("Scanning failed: ", error); 
+    }, function (error) {
+
+        console.log("Scanning failed: ", error);
+
+    },
+    {
+        "preferFrontCamera" : true, // iOS and Android
+        "showFlipCameraButton" : false, // iOS and Android
+        "prompt" : "Place a barcode inside the scan area", // supported on Android only
+        "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
     } );
 
 }
